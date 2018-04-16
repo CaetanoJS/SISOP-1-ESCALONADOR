8a6ecf6b71826c8fc4dfd494b96b2f98f0c43717
CaetanoJS <caetanoj.ss@gmail.com>
2018-04-16 10:55:43 -0300

    cthread exemples

---
+++ cthread/src/cthread.c
@@ -0,0 +1,24 @@
+#include "../include/support.h"
+#include "../include/cdata.h"
+
+int ccreate (void* (*start)(void*), void *arg, int prio){
+	
+
+	
+	return 1;
+}
+
+int cidentify (char *name, int size){
+    char id_grupo[IDENTIFY] = "Leonardo Eich 242314\nFelipe Fischer 242264\nCaetano Jaeger 242309\n";
+
+	if (size >= IDENTIFY){
+		strcpy(name, id_grupo);
+		return RETURN_OK;
+	}
+
+	return RETURN_ERROR;
+}
+
+int cyield(void){
+	return 0;
+}
